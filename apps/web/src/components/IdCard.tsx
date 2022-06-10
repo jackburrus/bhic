ea65d67f05fe12060183c91a63d0a1ce48f55c03
jack <jack@jpg.space>
2022-06-10 16:39:31 +0200

    id looking good

--- apps/web/src/components/IdCard.tsx
+++ apps/web/src/components/IdCard.tsx
@@ -3,8 +3,8 @@ import Webcam from 'react-webcam';
 
 export function IdCard() {
 	return (
-		<div className=" bg-[#D8B992] p-6 rounded-md flex flex-row w-[700px] h-96">
-			<div className="flex border border-orange-300">
+		<div className=" bg-[#f8f5de] p-6 rounded-md flex flex-row w-[700px] h-96">
+			<div className="flex border border-amber-200">
 				<div className="flex ml-1 -rotate-2 w-1/3 flex-col  items-start mr-[2px]">
 					<Webcam className="z-10" videoConstraints={{ width: 700, height: 800 }} />
 					<img
@@ -15,11 +15,11 @@ export function IdCard() {
 						src="/ethlogo.png"
 					/>
 				</div>
-				<div className="border-orange-500 p-[3px] border flex w-3/4 flex-col ">
-					<div className=" text-5xl border-b-2 border-orange-500 flex-1 flex">ID</div>
-					<div className="border-b-2 flex-1 border-orange-500 flex w-full">Name</div>
-					<div className="border-b-2 flex-1 border-orange-500 flex w-full">Age</div>
-					<div className="border-b-2 flex-1 border-orange-500 flex w-full">Gender</div>
+				<div className=" p-[8px]  flex w-3/4 flex-col ">
+					<div className=" text-5xl  border-b border-amber-300 flex-1 flex"> ID</div>
+					<div className="border-b flex-1 border-amber-300 flex w-full">Name</div>
+					<div className="border-b flex-1 border-amber-300 flex w-full">Age</div>
+					<div className="border-b flex-1 border-amber-300 flex w-full">Gender</div>
 				</div>
 			</div>
 		</div>
