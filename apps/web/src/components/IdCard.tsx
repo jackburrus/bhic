caa903710f13af6c88016c66163582df0e298b73
jack <jack@jpg.space>
2022-06-10 16:47:25 +0200

    burned edge looking good

--- apps/web/src/components/IdCard.tsx
+++ apps/web/src/components/IdCard.tsx
@@ -3,10 +3,10 @@ import Webcam from 'react-webcam';
 
 export function IdCard() {
 	return (
-		<div className=" bg-[#f8f5de] p-6 rounded-md flex flex-row w-[700px] h-96">
+		<div className=" bg-[#f8f5de] shadow-customInset p-6 rounded-md flex flex-row w-[700px] h-96">
 			<div className="flex border border-amber-200">
 				<div className="flex ml-1 -rotate-2 w-1/3 flex-col  items-start mr-[2px]">
-					<Webcam className="z-10" videoConstraints={{ width: 700, height: 800 }} />
+					<Webcam className="z-10 rounded-sm" videoConstraints={{ width: 700, height: 800 }} />
 					<img
 						className="absolute bottom-2 right-10 -rotate-[25deg] rounded-full "
 						alt="id-card"
@@ -16,7 +16,7 @@ export function IdCard() {
 					/>
 				</div>
 				<div className=" p-[8px]  flex w-3/4 flex-col ">
-					<div className=" text-5xl  border-b border-amber-300 flex-1 flex"> ID</div>
+					<div className=" text-5xl  border-b border-amber-300 flex-1 flex">ID</div>
 					<div className="border-b flex-1 border-amber-300 flex w-full">Name</div>
 					<div className="border-b flex-1 border-amber-300 flex w-full">Age</div>
 					<div className="border-b flex-1 border-amber-300 flex w-full">Gender</div>
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,1 +1,10 @@
-module.exports = require("config/tailwind.config");
+module.exports = require('config/tailwind.config');
+
+module.exports.theme = {
+	boxShadow: {
+		button: '0px 4px 15px 2px rgba(0, 0, 0, 0.04)',
+		input: '0px 24px 100px rgba(0, 0, 0, 0.05)',
+		link: '0 10px 15px -3px rgba(234, 51, 35, .2)',
+		customInset: ' inset 0 0 30px rgba(220,120,0,0.8) ',
+	},
+};
