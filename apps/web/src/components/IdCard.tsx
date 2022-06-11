70d449eef478f1ecb95e5c252801eb4abb443bb7
jack <jack@jpg.space>
2022-06-11 08:43:27 +0200

    models working

--- apps/web/src/components/IdCard.tsx
+++ apps/web/src/components/IdCard.tsx
@@ -1,12 +1,95 @@
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { useRef } from 'react';
 import Webcam from 'react-webcam';
+import * as faceapi from 'face-api.js';
+import { checkFace } from './checkFaces';
+import { useContractWrite, useAccount } from 'wagmi';
 
 export function IdCard() {
+	const webcamRef = useRef<Webcam>(null);
+	//fetch ethereum address
+
+	const [{ data: account }] = useAccount();
+
+	const [age, setAge] = useState(null);
+	const [gender, setGender] = useState(null);
+	const [mood, setMood] = useState(null);
+	const [isLoaded, setIsLoaded] = useState<boolean>(false);
+	const loadModels = async () => {
+		const MODEL_URL = '/models';
+		await Promise.all([
+			faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+			faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+			faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+			faceapi.nets.ageGenderNet.loadFromUri(MODEL_URL),
+			faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+		])
+			.then(() => {
+				console.log('its done');
+			})
+			.catch((err) => {
+				console.log('error: ', err);
+			});
+
+		// const detection = await faceapi
+		// .detectAllFaces(image, options)
+		// //.withFaceExpressions()
+		// .withAgeAndGender();
+	};
+
+	const loadWaiting = async () => {
+		return new Promise((resolve) => {
+			const timer = setInterval(() => {
+				if (webcamRef.current?.video?.readyState == 4) {
+					resolve(true);
+					clearInterval(timer);
+				}
+			}, 500);
+		});
+	};
+
+	const fetchAgeAndGender = async (video) => {
+		const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
+		const ageAndGender = await faceapi.predictAgeAndGender(video);
+		const expressions = await faceapi.recognizeFaceExpressions(video);
+		setAge(ageAndGender.age.toFixed(2));
+		setGender(ageAndGender.gender);
+		const finalExpression = Object.keys(expressions).reduce((a, b) => (expressions[a] > expressions[b] ? a : b));
+
+		setMood(finalExpression);
+		//find expressions with highest score
+
+		console.log(ageAndGender);
+		console.log(expressions);
+	};
+
+	const faceDetected = async () => {
+		await loadModels();
+		await loadWaiting();
+		if (webcamRef.current) {
+			console.log('running');
+			setIsLoaded(true);
+			const webcam = webcamRef.current.video as HTMLVideoElement;
+
+			webcam.width = webcam.videoWidth;
+			webcam.height = webcam.videoHeight;
+
+			const video = webcamRef.current.video;
+			// console.log(video);
+			fetchAgeAndGender(video);
+		}
+	};
+
+	useEffect(() => {
+		faceDetected();
+	}, []);
+
 	return (
 		<div className=" bg-[#f8f5de] shadow-customInset p-6 rounded-md flex flex-row w-[700px] h-96">
 			<div className="flex border border-amber-200">
 				<div className="flex ml-1 -rotate-2 w-1/3 flex-col  items-start mr-[2px]">
-					<Webcam className="z-10 rounded-sm" videoConstraints={{ width: 700, height: 800 }} />
+					<Webcam ref={webcamRef} className="z-10 rounded-sm" videoConstraints={{ width: 700, height: 800 }} />
 					<img
 						className="absolute bottom-2 right-10 -rotate-[25deg] rounded-full "
 						alt="id-card"
@@ -17,9 +100,23 @@ export function IdCard() {
 				</div>
 				<div className=" p-[8px]  flex w-3/4 flex-col ">
 					<div className=" text-5xl  border-b border-amber-300 flex-1 flex">ID</div>
-					<div className="border-b flex-1 border-amber-300 flex w-full">Name</div>
-					<div className="border-b flex-1 border-amber-300 flex w-full">Age</div>
-					<div className="border-b flex-1 border-amber-300 flex w-full">Gender</div>
+					<div className="border-b flex-1 border-amber-300 flex w-full items-start flex-col">
+						<div>Address</div>
+						<div>{account?.address}</div>
+					</div>
+					<div className="border-b flex-1 border-amber-300 flex w-full items-start flex-col">
+						<div>Age</div>
+						<div>{age}</div>
+					</div>
+
+					<div className="border-b flex-1 border-amber-300 flex w-full items-start flex-col">
+						<div>Gender</div>
+						<div>{gender}</div>
+					</div>
+					<div className="border-b flex-1 border-amber-300 flex w-full items-start flex-col">
+						<div>Mood</div>
+						<div>{mood}</div>
+					</div>
 				</div>
 			</div>
 		</div>
